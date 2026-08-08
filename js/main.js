c66c416e85b4bc9a6d889d19d97e290e2afb0773
Timofej Bliki <timofejbliki@gmail.com>
2026-08-08 13:46:28 +0200

    Added input conversion functions. * Also added .gitignore TODO: * Files such as filename.exe are misflagged as domains, check for domain TLDs and make FileName group * Add support for IPv6 simplification * Add support for registry keys, mutex, and user agents.

--- js/main.js
+++ js/main.js
@@ -5,80 +5,71 @@ iocField.addEventListener("input", updateKQL)
 
 
 function updateKQL(e) {
-    // kqlField.value = e.target.value;
-    if (isIpv4Address(e.target.value)) {
-        kqlField.value = "IPv4";
+    kqlField.value = formatCategorizedInput(categorizeInput(e.target.value, regexList), iocCategories);
+    // console.log(categorizeInput(e.target.value, regexList));
+}
+
+function categorizeInput(input, regexList) {
+    const words = (input.match(/\S+/g) || [])
+        .map(word => word.replace(/`/g, ""));
+    console.log(words)
+    const arrays = regexList.map(() => []);
+    for (const word of words) {
+        regexList.forEach((regex, i) => {
+            if (regex.test(refang(word))) {
+                arrays[i].push(refang(word));
+            }
+           // global (/g) or sticky regexes: reset position
+            regex.lastIndex = 0;
+        });
     }
-    else if (isIpv6Address(e.target.value)) {
-        kqlField.value = "IPv6";
+    return arrays;
+}
+
+function formatCategorizedInput(categorizedInput, categories) {
+    let output_str = ""
+    if (categorizedInput.length !== categories.length) {
+        throw new Error('Either too many or too little categories present.');
     }
-    else if (isMD5(e.target.value)) {
-        kqlField.value = "MD5";
+    for (let i = 0; i < categories.length; i += 1) {
+        if (categorizedInput[i].length > 0) {
+            let len = categorizedInput[i].length;
+            output_str += "let Malicious" + categories[i] + "s = pack_array(\n";
+            for (let j = 0; j < len - 1; j += 1) {
+                output_str += '    "' + categorizedInput[i][j] + '",\n';
+            }
+            output_str += '    "' + categorizedInput[i][len - 1] + '"\n);\n';
+        }
     }
-    else if (isSHA1(e.target.value)) {
-        kqlField.value = "SHA1";
-    }
-    else if (isSHA256(e.target.value)) {
-        kqlField.value = "SHA256";
-    }
-    else if (isDomain(e.target.value)) {
-        kqlField.value = "Domain";
-    }
-    else if (isURL(e.target.value)) {
-        kqlField.value = "URL";
-    }
-    else if (isEmail(e.target.value)) {
-        kqlField.value = "Email";
-    }
-    else {
-        kqlField.value = "";
-    }
+    return output_str;
 }
 
 function refang(input) {
     return input
-        .replace(/http:\/\//g, "hxxp://")
-        .replace(/https:\/\//g, "hxxps://")
+        .replace(/hxxp:\/\//g, "http://")
+        .replace(/hxxps:\/\//g, "https://")
         .replace(/\[\.\]/g, ".")
         .replace(/\[@\]/g, "@")
 }
 
-function isIpv4Address(input) {
-    const regexpIpv4 = /^(?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]\d|\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]\d|\d)){3}$/
-    return regexpIpv4.test(refang(input))
-}
+const regexList = [
+    /^(?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]\d|\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]\d|\d)){3}$/, // IPv4
+    /^(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$/, // Dumb IPv6 (no :: simplification)
+    /^[0-9a-fA-F]{32}$/, // MD5
+    /^[0-9a-fA-F]{40}$/, // SHA1
+    /^[0-9a-fA-F]{64}$/, // SHA256
+    /^(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,63}$/i, // Domain
+    /^https?:\/\/(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,63}(?::\d{1,5})?(?:\/[^\s]*)?$/i, // URL
+    /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,63}$/i, // Email
+]
 
-function isIpv6Address(input) {
-    const regexIpv6 = /^(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$/
-    return regexIpv6.test(refang(input))
-}
-
-function isMD5(input) {
-    const regexMD5 = /^[0-9a-fA-F]{32}$/
-    return regexMD5.test(refang(input))
-}
-
-function isSHA1(input) {
-    const regexSHA1 = /^[0-9a-fA-F]{40}$/
-    return regexSHA1.test(refang(input))
-}
-
-function isSHA256(input) {
-    const regexSHA256 = /^[0-9a-fA-F]{64}$/
-    return regexSHA256.test(refang(input))
-}
-
-function isDomain(input) {
-    const regexDomain = /^(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,63}$/i;
-    return regexDomain.test(refang(input))
-}
-
-function isURL(input) {
-    const regexUrl = /^https?:\/\/(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,63}(?::\d{1,5})?(?:\/[^\s]*)?$/i;
-    return regexUrl.test(refang(input))
-}
-
-function isEmail(input) {
-    const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,63}$/i;
-    return regexEmail.test(refang(input))
-}
+const iocCategories = [
+    "IPv4",
+    "IPv6",
+    "MD5",
+    "SHA1",
+    "SHA256",
+    "Domain",
+    "URL",
+    "Email"
+]
